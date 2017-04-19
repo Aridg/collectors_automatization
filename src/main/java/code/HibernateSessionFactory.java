bc0292dedea05937e40486ffee2d0cc51a2915a2
Aridg <rider-3x@yandex.ru>
2017-04-20 00:36:03 +0300

    create HibernateSessionFactory

--- src/main/java/code/HibernateSessionFactory.java
+++ src/main/java/code/HibernateSessionFactory.java
@@ -1,8 +1,26 @@
 package code;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Created by Aridg on 19.04.17.
  */
 public class HibernateSessionFactory {
+    private static final SessionFactory ourSessionFactory;
 
+    static {
+        try {
+            ourSessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() throws HibernateException {
+        return ourSessionFactory;
+    }
 }
