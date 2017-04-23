00183272f9d0c55fad2a84a96d42965cba09e0c1
Aridg <telerman.ae@post.ru>
2017-04-23 14:37:04 +0200

    fix

--- src/main/java/code/hibernate/HibernateSessionFactory.java
+++ src/main/java/code/hibernate/HibernateSessionFactory.java
@@ -5,9 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Created by Aridg on 19.04.17.
- */
+
 public class HibernateSessionFactory {
     private static final SessionFactory ourSessionFactory;
 
